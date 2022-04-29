61ddb19329f3a1bcf400f5d0d6f003579dc5851b
ALocke333 <104593944+ALocke333@users.noreply.github.com>
2022-04-29 08:42:59 -1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// my age 
+const myAge = 33;
+// first two years 
+var earlyYears = 2;
+var earlyYears = earlyYears * 10.5;
+// my age subtract first two years
+var laterYears = myAge - 2;
+// later years multiplied by 4 to get dog years
+var dogYears = laterYears * 4;
+console.log(laterYears);
+// early years plus lateryears to get age in dog years
+var myAgeInDogYears = earlyYears + laterYears ;
+const myName = 'Alex' ;
+// my name in lower case
+console.log('Alex'.toLowerCase());
+// my name and age in both human years and dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${dogYears} years old in dog years.`)
